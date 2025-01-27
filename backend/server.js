86980b863f4170e4bcc9583f8398db152f8bcf02
SirJames115 <81270423+SirJames115@users.noreply.github.com>
2025-01-27 14:57:35 +0100

    added the verify token function for routes protection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import colors from "colors";
 import authRoutes from "./routes/auth.route.js";
 
@@ -11,8 +13,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware to parse JSON body
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON body
+app.use(cookieParser()); //allows us to parse incoming cookie
 
 app.use(express.urlencoded({ extended: true }));
 
